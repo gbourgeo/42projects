25b2805d234c38cac7a28817a891f15f1aade233
Gilles BOURGEOIS <gbourgeo@e2r5p22.42.fr>
2017-03-17 06:19:45 +0100

    irc: tout roule

--- ft_irc/src/serveur/sv_err.c
+++ ft_irc/src/serveur/sv_err.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 04:01:19 by gbourgeo          #+#    #+#             */
-/*   Updated: 2017/03/17 06:09:18 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2017/03/17 06:18:36 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,30 @@ static int		sv_size(char *str)
 	return (i);
 }
 
-static void		do_this(char **reply, char *cmd, t_fd *cl)
+static void		do_this(char *reply, char *reply2, char *cmd, t_fd *cl)
 {
 	send(cl->fd, " ", 1, 0);
-	send(cl->fd, reply[0], sv_size(reply[0]), 0);
-	if (reply[1])
+	send(cl->fd, reply, sv_size(reply), 0);
+	if (reply2)
 	{
 		send(cl->fd, " ", 1, 0);
-		send(cl->fd, reply[1], sv_size(reply[1]), 0);
+		send(cl->fd, reply2, sv_size(reply2), 0);
 	}
-	else if (reply[0] == NULL)
+	else if (reply == NULL)
 	{
 		send(cl->fd, " ", 1, 0);
 		send(cl->fd, cmd, sv_size(cmd), 0);
 	}
 }
 
-static void		do_that(char **reply, char *cmd, char *cmd2, t_fd *cl)
+static void		do_that(char *reply, char *cmd, char *cmd2, t_fd *cl)
 {
 	send(cl->fd, " ", 1, 0);
 	send(cl->fd, cmd, sv_size(cmd), 0);
-	if (reply[1])
+	if (reply)
 	{
 		send(cl->fd, " ", 1, 0);
-		send(cl->fd, reply[1], sv_size(reply[1]), 0);
+		send(cl->fd, reply, sv_size(reply), 0);
 		if (cmd2)
 		{
 			send(cl->fd, " ", 1, 0);
@@ -68,8 +68,8 @@ void			sv_err(char *err, char *cmd, char *cmd2, t_fd *cl)
 
 	if (ft_strlen(err) != 3)
 		return ;
-	pos = ft_atoi(err) - 400;
-	if (pos > 0 && pos < 102)
+	pos = ft_atoi(err) - 401;
+	if (pos >= 0 && pos < 102)
 	{
 		send(cl->fd, e.name, SERVER_LEN, 0);
 		send(cl->fd, " ", 1, 0);
@@ -77,9 +77,9 @@ void			sv_err(char *err, char *cmd, char *cmd2, t_fd *cl)
 		send(cl->fd, " ", 1, 0);
 		send(cl->fd, cl->reg.nick, NICK_LEN, 0);
 		if (replies[pos][0])
-			do_this(replies[pos], cmd, cl);
+			do_this(replies[pos][0], replies[pos][1], cmd, cl);
 		else if (cmd)
-			do_that(replies[pos], cmd, cmd2, cl);
+			do_that(replies[pos][1], cmd, cmd2, cl);
 		if (replies[pos][2])
 		{
 			send(cl->fd, " ", 1, 0);
