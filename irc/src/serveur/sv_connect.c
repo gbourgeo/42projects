85cb51e2ee637a7979fbc0478f9e209ef06399e1
gbourgeo <gillesbourgeoi@gmail.com>
2017-04-10 11:22:57 +0200

    irc: no connect to another server

--- irc/src/serveur/sv_connect.c
+++ irc/src/serveur/sv_connect.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 00:30:13 by gbourgeo          #+#    #+#             */
-/*   Updated: 2017/04/03 21:03:45 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2017/04/10 11:20:47 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void			sv_connect(char **cmds, t_env *e, t_fd *cl)
 	if (!cmds[0] || !*cmds[0] || !cmds[1] || !*cmds[1])
 		return (sv_err(ERR_NEEDMOREPARAMS, "CONNECT", NULL, cl));
 	if (cl->inf->umode & USR_OP || cl->inf->umode & USR_LOCALOP)
-		return ;
+		return (sv_notice("You are already connected to a server.", cl));
 	sv_err(ERR_NOPRIVILEGES, "CONNECT", NULL, cl);
 }
