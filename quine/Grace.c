9485d86b64e97536f68eb25f07159091a1a2ccd3
Gilles BOURGEOIS <gbourgeo@e3r3p21.42.fr>
2017-12-17 22:29:21 +0100

    Dr_quine: C and C++

--- quine/Grace.c
+++
@@ -1,9 +0,0 @@
-/*
-  This program will print its code source in a file only using macros.
-*/
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#define OPEN int fd = open("Grace_kid.c",O_CREAT|O_TRUNC|O_WRONLY,0644); if (fd < 0) return -1;
-#define MAIN int main(void){ OPEN char*str="/*%c  This program will print its code source in a file only using macros.%c*/%c#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define OPEN int fd = open(%cGrace_kid.c%c,O_CREAT|O_TRUNC|O_WRONLY,0644); if (fd < 0) return -1;%c#define MAIN int main(void){ OPEN char*str=%c%s%c;dprintf(fd, str, 10,10,10, 10,10,10, 34,34,10, 34,str,34, 10, 10);close(fd);return 0;}%cMAIN%c";dprintf(fd, str, 10,10,10, 10,10,10, 34,34,10, 34,str,34, 10, 10);close(fd);return 0;}
-MAIN
