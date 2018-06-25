6bbb77f7058826353f66040b0c7377aa32589668
gbourgeo <gillesbourgeois@gmail.com>
2018-06-25 13:08:22 +0200

    quine: C: project specification modification

--- quine/ASM/Colleen.s
+++ quine/ASM/Colleen.s
@@ -2,7 +2,7 @@
 	global main
 
 	section .rodata
-	string db "	extern printf%c	global main%c%c	section .rodata%c	string db %c%s%c, 0%c%c	section .text%c	;; Here begins the main function%cmain:%c	push rbp%c	mov rbp, rsp%c	lea rdi, [rel string]%c	mov rsi, 10%c	mov rdx, 10%c	mov rcx, 10%c	mov r8, 10%c	mov r9, 34%c", 0
+	string db "	extern printf%c	global main%c%c	section .rodata%c	string db %c%s%c, 0%c%c	section .text%c	;; Here begins the main function%cmain:%c	push rbp%c	mov rbp, rsp%c	lea rdi, [rel string]%c	mov rsi, 10%c	mov rdx, 10%c	mov rcx, 39%c	mov r8, 10%c	mov r9, 34%cloop:%c	push 10%c	call dec_rcx%c	cmp rcx, 10%c	jg loop%c	push 34%c	push rdi%c	xor rax, rax%c	call printf WRT ..plt%c	xor rax, rax%c	leave%c	ret%c%cdec_rcx:%c	dec rcx%c	ret%c", 0
 
 	section .text
 	;; Here begins the main function
@@ -12,29 +12,22 @@ main:
 	lea rdi, [rel string]
 	mov rsi, 10
 	mov rdx, 10
-	mov rcx, 10
+	mov rcx, 39
 	mov r8, 10
 	mov r9, 34
+loop:
 	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
-	push 10
+	call dec_rcx
+	cmp rcx, 10
+	jg loop
 	push 34
 	push rdi
 	xor rax, rax
 	call printf WRT ..plt
 	xor rax, rax
 	leave
+	ret
+
+dec_rcx:
+	dec rcx
 	ret
--- quine/C/Colleen.c
+++ quine/C/Colleen.c
@@ -2,12 +2,16 @@
 /*
 	This program prints its own code source.
 */
+static void print(char *str)
+{
+	printf(str, 10, 10, 9,10, 10, 10, 10, 9,10, 10, 10, 10, 9,10, 9,10, 9,10, 9,34,str,34, 10, 9,10, 9,10, 10);
+}
 int main(void)
 {
 	/*
 	  This is what we call a quine.
 	*/
-	char *str = "#include <stdio.h>%c/*%c%cThis program prints its own code source.%c*/%cint main(void)%c{%c%c/*%c%c  This is what we call a quine.%c%c*/%c%cchar *str = %c%s%c;%c%cprintf(str, 10, 10,9, 10, 10, 10, 10,9, 10,9, 10,9, 10,9, 34,str,34, 10,9, 10,9, 10, 10);%c%creturn (0);%c}%c";
-	printf(str, 10, 10,9, 10, 10, 10, 10,9, 10,9, 10,9, 10,9, 34,str,34, 10,9, 10,9, 10, 10);
+	char *str = "#include <stdio.h>%c/*%c%cThis program prints its own code source.%c*/%cstatic void print(char *str)%c{%c%cprintf(str, 10, 10, 9,10, 10, 10, 10, 9,10, 10, 10, 10, 9,10, 9,10, 9,10, 9,34,str,34, 10, 9,10, 9,10, 10);%c}%cint main(void)%c{%c%c/*%c%c  This is what we call a quine.%c%c*/%c%cchar *str = %c%s%c;%c%cprint(str);%c%creturn (0);%c}%c";
+	print(str);
 	return (0);
 }
