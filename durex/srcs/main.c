39e28b4e9828eee992d1b648ebef288cb414673d
GBO <gbourgeo@student.42.fr>
2018-07-29 11:04:35 +0200

    changed executable launch permission and installation

--- durex/srcs/main.c
+++ durex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/28 08:13:10 by root              #+#    #+#             */
-/*   Updated: 2018/07/22 12:03:54 by root             ###   ########.fr       */
+/*   Updated: 2018/07/29 09:17:58 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,14 @@ static void		print_usr_name()
 
 int				main(int ac, char **av)
 {
+	struct stat	s;
+
 	(void)ac;
 	print_usr_name();
-	if (setuid(getuid()) != 0)
+	if (seteuid(geteuid()) != 0)
 		return 3;
-	if (install_binary(av[0]))
-		return 3;
+	if (!ac && stat(DUREX_BINARY_FILE, &s) < 0)
+		return install_binary(av[0]);
 	e.lock = open(DUREX_LOCK_FILE, O_CREAT | O_WRONLY, 0600);
 	if (e.lock < 0)
 		return 3;
