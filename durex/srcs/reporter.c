c876aaf606dcab98349641fecdb13bde355287a4
GBO <gbourgeo@student.42.fr>
2018-08-14 02:02:07 +0200

    reporter supports %d %s %p

--- durex/srcs/reporter.c
+++ durex/srcs/reporter.c
@@ -6,7 +6,7 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 22:47:39 by root              #+#    #+#             */
-/*   Updated: 2018/08/06 19:35:08 by root             ###   ########.fr       */
+/*   Updated: 2018/08/14 01:35:29 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,66 @@
 #include <time.h>
 /* write */
 #include <unistd.h>
+/* ... */
+#include <stdarg.h>
+/* malloc */
+#include <stdlib.h>
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-static int		len(const char *s)
+#include "main.h"
+
+int				hireReporter()
 {
-	char		*p = (char *)s;
-
-	while (*p)
-		p++;
-	return (p - s);
+	return open(SERVER_REPORTER, O_CREAT | O_TRUNC | O_RDWR, 0600);
 }
 
-void			serverLog(int fd, const char *message)
+void			serverLog(const char *message, ...)
 {
 	time_t		t;
 	struct tm	*tm;
 	char		buf[25];
+	va_list		ap;
+	size_t		i, j;
 
-	if (fd < 0)
+	if (e.server.reporter < 0)
 		return ;
 	t = time(NULL);
-	tm = localtime(&t);	
+	tm = localtime(&t);
 	strftime(buf, 25, "[%d/%m/%Y - %H:%M:%S] ", tm);
-	write(fd, buf, len(buf));
-	write(fd, message, len(message));
-	write(fd, "\n", 1);
+	write(e.server.reporter, buf, mystrlen(buf));
+	va_start(ap, message);
+	i = j = 0;
+	while (message[i]) {
+		if (message[i] == '%') {
+			if (i != j)
+				write(e.server.reporter, &message[j], i - j);
+			j = i + 2;
+			if (message[i+1] == 'd') {
+				char *ptr;
+				if ((ptr = myitoa(va_arg(ap, int))) != NULL) {
+					write(e.server.reporter, ptr, mystrlen(ptr));
+					free(ptr);
+				}
+			} else if (message[i+1] == 's') {
+				char *ptr = va_arg(ap, char *);
+
+				write(e.server.reporter, ptr, mystrlen(ptr));
+			} else if (message[i+1] == 'p') {
+				char *ptr;
+				if ((ptr = myitoa(va_arg(ap, unsigned long long))) != NULL) {
+					write(e.server.reporter, ptr, mystrlen(ptr));
+					free(ptr);
+				}
+			} else {
+				write(e.server.reporter, &message[i], 2);
+			}
+			i++;
+		}
+		i++;
+	}
+	write(e.server.reporter, &message[j], i - j);
+	write(e.server.reporter, "\n", 1);
 }
