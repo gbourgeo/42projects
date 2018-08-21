9af92aebd7f6561b0d14e6488c7e63fa4c29a9c3
gbourgeo <gillesbourgeois@gmail.com>
2018-08-21 10:36:47 +0200

    add split whitespaces to durex stdlib

--- durex/srcs/stdlib.c
+++ durex/srcs/stdlib.c
@@ -6,7 +6,7 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 15:02:59 by root              #+#    #+#             */
-/*   Updated: 2018/08/14 00:41:58 by root             ###   ########.fr       */
+/*   Updated: 2018/08/21 09:55:05 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,4 +68,72 @@ char			*myitoa(int n)
 		src /= 10;
 	}
 	return (p);
+}
+
+static int		tab_lines(char *s)
+{
+	int			i;
+	int			lines;
+
+	i = 0;
+	lines = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != ' ' && (s[i] < 9 || s[i] > 11))
+		{
+			lines += 1;
+			while (s[i] != ' ' && (s[i] < 9 || s[i] > 11) && s[i] != '\0')
+				++i;
+		}
+		else
+			++i;
+	}
+	return (lines);
+}
+
+static char		*fill_tab(char *str)
+{
+	int			i;
+	char		*copy;
+
+	i = 0;
+	copy = NULL;
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != ' ' && (str[i] < 9 || str[i] > 11) && str[i] != '\0')
+		i += 1;
+	if ((copy = (char*)malloc(sizeof(copy) * (i + 1))) == NULL)
+		return (NULL);
+	copy[i] = '\0';
+	while (--i >= 0)
+		copy[i] = str[i];
+	return (copy);
+}
+
+char			**mysplitwhitespaces(char *str)
+{
+	int			i;
+	int			j;
+	char		**tab;
+
+	i = 0;
+	j = 0;
+	if (str == NULL)
+		return (NULL);
+	if ((tab = (char**)malloc(sizeof(*tab) * (tab_lines(str) + 1))) == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && (str[i] < 9 || str[i] > 11))
+		{
+			if ((tab[j++] = fill_tab(str + i)) == NULL)
+				break ;
+			while (str[i] != ' ' && (str[i] < 9 || str[i] > 11) && str[i])
+				++i;
+		}
+		else
+			++i;
+	}
+	tab[j] = NULL;
+	return (tab);
 }
