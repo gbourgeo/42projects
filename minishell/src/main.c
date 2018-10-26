ddb98b71ff7d9e12c8688013eec481924ee8d020
GBO <gbourgeo@student.42.fr>
2018-10-26 09:14:25 +0200

    add ft_signal() func

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/28 02:04:29 by gbourgeo          #+#    #+#             */
-/*   Updated: 2018/10/20 21:09:04 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2018/10/26 08:47:17 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void		init_env(const char *prog, const char **environ, t_env *e)
 	i = 0;
 	e->progname = ft_strrchr(prog, '/');
 	e->progname = (e->progname == NULL) ? prog : e->progname + 1;
-	e->env = ft_envdup(environ);
+	e->env = ft_envdup(prog, environ);
 	if (e->env == NULL)
 		ft_fatal("Can't copy environnement.", e);
 	path = ft_strsplit(ft_getenv("PATH", e->env), ':');
@@ -42,12 +42,11 @@ static void		init_env(const char *prog, const char **environ, t_env *e)
 
 int				main(int ac, char **av, const char **environ)
 {
-	t_env		e;
-
 	(void)ac;
 	(void)av;
 	ft_memset(&e, 0, sizeof(e));
 	init_env(av[0], environ, &e);
+	ft_signals();
 	ft_shell(&e);
 	ft_exit(NULL, &e);
 	return (0);
