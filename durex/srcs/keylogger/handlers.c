8d2b678cf81ff5e1d0d61d042ad3f358c2e948aa
GBO <gbourgeo@student.42.fr>
2018-11-15 19:45:04 +0100

    fixed return values on handlers

--- durex/srcs/keylogger/handlers.c
+++ durex/srcs/keylogger/handlers.c
@@ -1,9 +1,7 @@
-//#include "keylogger.h"
-
-int             shift_handler(int value, int state, int *modifier)
+int		shift_handler(int value, int state, int *modifier)
 {
 	if (state == 2)
-		return 0;
+		return 1;
 	if (state == 0)
 		*modifier -= (1 << value);
 	else if (state == 1)
@@ -11,10 +9,10 @@ int             shift_handler(int value, int state, int *modifier)
 	return 1;
 }
 
-int				lock_handler(int value, int state, int *modifier, int *locker)
+int		lock_handler(int value, int state, int *modifier, int *locker)
 {
 	if (state != 1)
-		return 0;
+		return 1;
 	locker[value] = !(locker[value]);
 	if (locker[value] == 0)
 		*modifier -= (1 << value);
