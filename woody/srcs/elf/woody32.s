32c6061735ccde0c88736daa52e39c528e2a9bca
naminei <naminei@natachas-macbook-pro.home>
2019-06-26 17:37:08 +0200

    [WOODY] SEGV on _start

--- woody/srcs/elf/woody32.s
+++ woody/srcs/elf/woody32.s
@@ -29,109 +29,111 @@ woody32_func:					; ELF 32 bits version
 	int		0x80
 
 	jmp 	woody32_end
+
 .get_eip:
 	mov		eax, [esp]
 	ret
-woody32_decrypt:
-	push	ebp
-	push	edi
-	push	esi
-	push	ebx
-	sub		esp, 24
 
-	mov		eax, DWORD [esp+48]
-	and		eax, -8
-	mov		DWORD [esp+20], eax
-	je		.L8
-	mov		eax, DWORD [esp+44]
-	mov		DWORD [esp+12], eax
-	mov		DWORD [esp+8], 0
-.L11:
-	mov		esi, DWORD [esp+12]
-	mov		DWORD [esp+16], esi
-	movzx	ecx, BYTE [esi]
-	sal		ecx, 24
-	movzx	eax, BYTE [esi+1]
-	sal		eax, 16
-	or		ecx, eax
-	movzx	eax, BYTE [esi+3]
-	or		ecx, eax
-	movzx	eax, BYTE [esi+2]
-	sal		eax, 8
-	or		ecx, eax
-	movzx	edx, BYTE [esi+4]
-	mov		eax, edx
-	sal		eax, 24
-	movzx	edx, BYTE [esi+5]
-	sal		edx, 16
-	or		edx, eax
-	movzx	eax, BYTE [esi+7]
-	or		edx, eax
-	movzx	eax, BYTE [esi+6]
-	sal		eax, 8
-	or		edx, eax
-	mov		eax, DWORD [esp+52]
-	mov		eax, DWORD [eax]
-	mov		DWORD [esp], eax
-	mov		eax, DWORD [esp+52]
-	mov		eax, DWORD [eax+4]
-	mov		DWORD [esp+4], eax
-	mov		eax, DWORD [esp+52]
-	mov		edi, DWORD [eax+8]
-	mov		esi, DWORD [eax+12]
-	mov		ebx, -957401312
-.L10:
-	mov		eax, ecx
-	sal		eax, 4
-	lea		ebp, [eax+edi]
-	mov		eax, ecx
-	shr		eax, 5
-	add		eax, esi
-	xor		eax, ebp
-	lea		ebp, [ecx+ebx]
-	xor		eax, ebp
-	sub		edx, eax
-	mov		eax, edx
-	sal		eax, 4
-	mov		ebp, DWORD [esp]
-	add		ebp, eax
-	mov		eax, edx
-	shr		eax, 5
-	add		eax, DWORD [esp+4]
-	xor		eax, ebp
-	lea		ebp, [edx+ebx]
-	xor		eax, ebp
-	sub		ecx, eax
-	add		ebx, 1640531527
-	jne		.L10
-	mov		eax, ecx
-	shr		eax, 24
-	mov		edi, DWORD [esp+16]
-	mov		BYTE [edi], al
-	mov		eax, ecx
-	shr		eax, 16
-	mov		BYTE [edi+1], al
-	mov		BYTE [edi+2], ch
-	mov		BYTE [edi+3], cl
-	mov		eax, edx
-	shr		eax, 24
-	mov		BYTE [edi+4], al
-	mov		eax, edx
-	shr		eax, 16
-	mov		BYTE [edi+5], al
-	mov		BYTE [edi+6], dh
-	mov		BYTE [edi+7], dl
-	add		DWORD [esp+8], 8
-	mov		eax, DWORD [esp+8]
-	add		DWORD [esp+12], 8
-	cmp		DWORD [esp+20], eax
-	ja		.L11
-.L8:
-	add		esp, 24
-	pop		ebx
-	pop		esi
-	pop		edi
-	pop		ebp
+woody32_decrypt:
+	push    ebp
+	push    edi
+	push    esi
+	push    ebx
+
+	sub     esp, 20
+	mov     eax, DWORD [esp + 44]
+	and     eax, -8
+	mov     DWORD [esp + 16], eax
+	je      .end
+	mov     eax, DWORD [esp + 40]
+	mov     DWORD [esp + 8], 0
+	mov     DWORD [esp + 12], eax
+.loop:
+	mov     edi, DWORD [esp + 12]
+	mov     ebx, -957401312
+	movzx   ecx, BYTE [edi]
+	movzx   eax, BYTE [edi + 1]
+	movzx   edx, BYTE [edi + 4]
+	sal     eax, 16
+	sal     ecx, 24
+	or      ecx, eax
+	movzx   eax, BYTE [edi + 3]
+	sal     edx, 24
+	or      ecx, eax
+	movzx   eax, BYTE [edi + 2]
+	sal     eax, 8
+	or      ecx, eax
+	movzx   eax, BYTE [edi + 5]
+	sal     eax, 16
+	or      edx, eax
+	movzx   eax, BYTE [edi + 7]
+	or      edx, eax
+	movzx   eax, BYTE [edi + 6]
+	sal     eax, 8
+	or      edx, eax
+	mov     eax, DWORD [esp + 48]
+	mov     eax, DWORD [eax]
+	mov     DWORD [esp], eax
+	mov     eax, DWORD [esp + 48]
+	mov     eax, DWORD [eax + 4]
+	mov     DWORD [esp + 4], eax
+	mov     eax, DWORD [esp + 48]
+	mov     edi, DWORD [eax + 8]
+	mov     esi, DWORD [eax + 12]
+.algorythm:
+	mov     eax, ecx
+	sal     eax, 4
+	lea     ebp, [eax + edi]
+	mov     eax, ecx
+	shr     eax, 5
+	add     eax, esi
+	xor     eax, ebp
+	lea     ebp, [ecx + ebx]
+	xor     eax, ebp
+	mov     ebp, DWORD [esp]
+	sub     edx, eax
+	mov     eax, edx
+	sal     eax, 4
+	add     ebp, eax
+	mov     eax, edx
+	shr     eax, 5
+	add     eax, DWORD [esp + 4]
+	xor     eax, ebp
+	lea     ebp, [edx + ebx]
+	xor     eax, ebp
+	sub     ecx, eax
+	add     ebx, 1640531527
+	jne     .algorythm
+	mov     esi, DWORD [esp + 12]
+	mov     eax, ecx
+	add     DWORD [esp + 8], 8
+	shr     eax, 24
+	mov     BYTE [esi], al
+	mov     eax, ecx
+	shr     eax, 16
+	mov     BYTE [esi + 2], ch
+	mov     BYTE [esi + 1], al
+	mov     eax, edx
+	shr     eax, 24
+	mov     BYTE [esi + 3], cl
+	mov     BYTE [esi + 4], al
+	mov     eax, edx
+	shr     eax, 16
+	mov     BYTE [esi + 6], dh
+	mov     BYTE [esi + 5], al
+	mov     eax, esi
+	add     eax, 8
+	mov     BYTE [esi + 7], dl
+	mov     esi, DWORD [esp + 8]
+	mov     DWORD [esp + 12], eax
+	cmp     DWORD [esp + 16], esi
+	ja      .loop
+.end:
+	add     esp, 20
+	pop     ebx
+	pop     esi
+	pop     edi
+	pop     ebp
 	ret
 
 woody32_end:
@@ -141,29 +143,36 @@ woody32_end:
 ; Put [woody32_keys] address into edx
 	mov		edx, ecx
 	sub		edx, 32
-; Put [woody32_func] into edi
+; Put [woody32_func] address into edi
 	mov		edi, edx
-	sub		edi, 0x19c
-; Add [text_vaddr] value to it
-	mov		ebx, ecx
-	sub		ebx, 16
-	add		edi, [ebx]
-mov eax,[banner32]
+	sub		edi, 0x1a1
+; Add [text_vaddr] offset to it
+	add		edi, [ecx - 16]
+
+	add		esp, 12
+	push	edx
+	push	esi
+	push	edi
 	call 	woody32_decrypt
 
-	lea 	ebx, [woody32_func]
-	add 	ebx, [text_vaddr]
-	mov 	[text_vaddr], ebx
-	lea 	eax, [woody32_func]
-	add 	eax, [jump_vaddr]
-	mov 	[jump_vaddr], eax
+;	lea 	ebx, [woody32_func]
+;	add 	ebx, [text_vaddr]
+;	mov 	[text_vaddr], ebx
+
+; Get [jump_vaddr] address
+	add		[esp + 8], 24
+; Move [woody32_func] address into EBX
+	mov		ebx, [esp + 16]
+	add		ebx, [ecx]
+	mov		[ecx], ebx
 
 	pop 	ebx
 	pop 	eax
 	pop 	edx
 	pop 	esi
 	pop 	edi
-	push	DWORD [jump_vaddr]
+	push	DWORD [ecx]
+;mov eax, [banner32]
 	ret
 
 woody32_data:
